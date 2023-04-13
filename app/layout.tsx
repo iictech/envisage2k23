0bd1b4c32ba86927b3df426596935316986f5e5e
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-13 11:23:01 +0530

    feat(app): add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+"use client";
+
+import { AuthContextProvider } from "@/context/AuthContext";
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full bg-gray-50">
+      <head />
+      <body className="h-full">
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </body>
     </html>
   );
 }
