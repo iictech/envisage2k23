741d030c3e47307ae5e63c537decfe745fc0893f
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-13 11:26:31 +0530

    feat(app): add page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import GoogleLogout from "@/components/GoogleLogout";
+import GoogleSignIn from "@/components/GoogleSignIn";
+import { useAuthContext } from "@/context/AuthContext";
+import { User } from "firebase/auth";
+
 export default function Home() {
+  const { user } = useAuthContext() as { user: User | null };
+
   return (
     <main>
-      <h1 className="text-lg font-bold">Hello World!</h1>
+      <h1 className="text-xl font-bold">Home</h1>
+      {!user ? <GoogleSignIn text="Sign in with Google" /> : <GoogleLogout />}
     </main>
   );
 }
