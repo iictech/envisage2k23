2e56de46f91272d6ce0626d5310e122c0b1bf67d
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-13 11:22:37 +0530

    feat(context): add AuthProvider context

---
+++ context/AuthContext.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import { app } from "@/firebase/config";
+import Loading from "@/app/components/loading";
+
+const auth = getAuth(app);
+
+export const AuthContext = React.createContext({});
+
+export const useAuthContext = () => React.useContext(AuthContext);
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
+  // https://stackoverflow.com/questions/59125973/react-typescript-argument-of-type-is-not-assignable-to-parameter-of-type
+  // https://stackoverflow.com/questions/75438048/cant-resolve-encoding-module-error-while-using-nextjs-13-supabase
+  const [user, setUser] = React.useState<User | null>(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user as User);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {loading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
+};
