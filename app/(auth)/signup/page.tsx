6f7320b0b7adf949148915ad627447e5130fbf35
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-15 21:40:32 +0530

    feat(auth): updated signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import Logo from "@/app/components/Logo";
 import GoogleButton from "@/app/components/buttons/GoogleButton";
 import { useAuthContext } from "@/context/AuthContext";
 import { User } from "firebase/auth";
@@ -18,22 +19,10 @@ export default function Home() {
         <title>Sign Up - Envisage 23</title>
         <main className="flex h-full w-screen bg-gray-100">
           <div className="flex w-full flex-col bg-white sm:m-auto sm:max-w-sm sm:rounded-xl sm:shadow-xl">
-            <div className="h-48 w-full bg-gradient-to-bl from-cyan-300 to-blue-700 sm:rounded-t-xl lg:h-56" />
-            <div className="mx-auto -mt-16 max-w-5xl flex-col px-4 sm:flex sm:items-end sm:space-x-5 sm:px-6 lg:px-8">
-              <div className="group relative mx-auto h-32 w-32 overflow-hidden rounded-full">
-                <img
-                  alt="Profile Picture"
-                  src={"https://source.unsplash.com/random/100x100/?abstract"}
-                  width="300"
-                  height="300"
-                  decoding="async"
-                  data-nimg="1"
-                  className="scale-100 blur-0 grayscale-0 transition-all"
-                  loading="lazy"
-                />
-              </div>
+            <div className="mx-auto max-w-5xl flex-col px-4 pt-36 sm:flex sm:items-end sm:space-x-5 sm:px-6 sm:pt-12 lg:px-8">
+              <Logo className="mx-auto h-32" />
               <div>
-                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+                <h2 className="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900">
                   Welcome to Envisage 23,
                 </h2>
               </div>
@@ -41,7 +30,6 @@ export default function Home() {
             <div className="mx-auto px-8">
               <div className="mb-12 mt-6 flex flex-col items-center justify-center">
                 <GoogleButton text="Sign Up with Google" />
-
                 <p className="mt-4 text-center text-sm text-gray-400 sm:text-xs">
                   By continuing, you agree to our
                   <a
