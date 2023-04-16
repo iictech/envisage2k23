2db00417a0c1046742af9fe7aa234bd9b881fbf0
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-17 00:38:34 +0530

    feat(home): update layout

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -7,7 +7,7 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
     <>
       <div className="bg-white">
         <NavBar />
-        <div className="relative isolate pt-20 sm:pt-24 lg:px-8">{children}</div>
+        <div className="relative isolate pt-20">{children}</div>
       </div>
     </>
   );
