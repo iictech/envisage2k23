50a9c778f9fe37a76ad75be467a2e8d30f734d9b
Subhadeep Roy <subha9.5roy350@gmail.com>
2023-04-20 06:25:21 +0000

    Updated Partners

--- app/(landing)/partners/page.tsx
+++ app/(landing)/partners/page.tsx
@@ -10,15 +10,15 @@ export default function PartnersPage() {
       <div className="all overflow-x-hidden">
         <Title title="Partner with Us" />
         <section className="mx-auto max-w-5xl px-4 py-20 sm:pt-10">
-          <div className="mx-auto mb-10 flex flex-col bg-gray-900 items-center gap-y-4 rounded-3xl p-10 text-black transition-all duration-200 ease-in-out hover:scale-95 hover:shadow-2xl hover:ring-4 hover:ring-blue-500 hover:ring-offset-8 sm:flex-row">
+          <div className="mx-auto mb-10 flex flex-col bg-gray-900 items-center gap-y-4 rounded-3xl p-10 text-black transition-all duration-200 ease-in-out hover:scale-95 hover:shadow-2xl hover:ring-4 hover:ring-blue-900 hover:ring-offset-8 sm:flex-row">
             <div className="sm:h-84 sm:w-84 inline-flex h-72 w-72 flex-shrink-0 items-center justify-center">
               <Logo className="h-60 w-60" />
             </div>
             <div className="mt-6 flex-grow text-center sm:mt-0 sm:text-left">
-              <h2 className="mb-6 text-4xl font-bold tracking-tight sm:text-5xl">
+              <h2 className="mb-6 text-4xl text-blue-300 font-bold tracking-tight sm:text-5xl">
                 What is Envisage?
               </h2>
-              <p className="mx-auto mt-6 text-lg leading-8 text-zinc-600 sm:text-left sm:text-xl">
+              <p className="mx-auto mt-6 text-lg leading-8 text-white sm:text-left sm:text-xl">
                 Envisage is the official E-Summit of Techno Main Salt Lake. It is born out
                 of the dreams and aspirations of the youth who have the zest and the zeal
                 to take their future into their own hands. Supporting the growing startup
@@ -35,7 +35,7 @@ export default function PartnersPage() {
               {events.map((event) => (
                 <div
                   key={event.id}
-                  className="cursor-pointer rounded-md border bg-white p-4 text-center text-lg font-semibold shadow transition-all duration-200 ease-in-out hover:bg-blue-500 hover:text-white hover:shadow-xl"
+                  className="cursor-pointer rounded-md border bg-white p-4 text-center text-lg font-semibold shadow transition-all duration-200 ease-in-out hover:bg-gray-900 hover:text-white hover:shadow-xl"
                 >
                   {event.title}
                 </div>
@@ -43,7 +43,7 @@ export default function PartnersPage() {
             </div>
           </div>
           <div className="flex justify-center py-12">
-            <button className="block rounded-lg bg-blue-600 px-6 py-4 text-center text-base font-semibold uppercase tracking-tight text-zinc-100 active:scale-95 sm:px-12 sm:text-xl">
+            <button className="block rounded-lg bg-gray-900 px-6 py-4 text-center text-base font-semibold uppercase tracking-tight text-zinc-100 active:scale-95 sm:px-12 sm:text-xl">
               Join as a Event Partner
             </button>
           </div>
