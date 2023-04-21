9b842a56baaad75e011b831525802f74223c83aa
Subhadeep Roy <subha9.5roy350@gmail.com>
2023-04-21 07:55:25 +0000

    fixed abt us

--- app/(landing)/about/page.tsx
+++ app/(landing)/about/page.tsx
@@ -169,7 +169,7 @@ export default function AboutPage() {
             </ol>
           </div>
           
-          <div>
+          <div className="mt-14 mx-auto mb-10 flex flex-col items-center gap-y-4 rounded-3xl bg-gray-900 p-10 text-white transition-all duration-200 ease-in-out hover:scale-95 hover:shadow-2xl hover:ring-4 hover:ring-blue-900 hover:ring-offset-8 sm:flex-row">
             <Image src={Events} alt=""/>
           </div>
         </section>
