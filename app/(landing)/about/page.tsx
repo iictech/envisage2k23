8657c5c83cb48ac9ed577db55b554bf2f775df4c
Subhadeep Roy <subha9.5roy350@gmail.com>
2023-04-21 07:36:00 +0000

    Updated about us page

--- app/(landing)/about/page.tsx
+++ app/(landing)/about/page.tsx
@@ -48,12 +48,12 @@ export default function AboutPage() {
           </div>
 
 
-          <div className="m-auto justify-center items-center">
+          <div className="flex mt-14 justify-center items-center">
             <h2 className="mb-6 text-4xl font-bold tracking-tight sm:text-5xl">
               Events Timeline
             </h2>
           </div>
-          <div className="mt-14">
+          <div className="mt-10">
             <ol className="items-center sm:flex">
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
@@ -65,7 +65,7 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
@@ -78,7 +78,7 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
@@ -91,15 +91,41 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
+                </div>
+              </li>
+              <li className="relative mb-6 sm:mb-0">
+                <div className="flex items-center">
+                  <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                </div>
+                <div className="mt-4 sm:pr-8">
+                  <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
+                  <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
             </ol>
           </div>
 
 
-          <div className="mt-14">
+          <div className="mt-10">
             <ol className="items-center sm:flex">
+              <li className="relative mb-6 sm:mb-0">
+                <div className="flex items-center">
+                  <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                    <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                </div>
+                <div className="mt-4 sm:pr-8">
+                  <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
+                  <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
+                </div>
+              </li>
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
                   <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
@@ -110,7 +136,7 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY1</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
@@ -123,7 +149,7 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
@@ -136,7 +162,7 @@ export default function AboutPage() {
                 <div className="mt-4 sm:pr-8">
                   <h3 className="text-2xl font-bold text-white">Inauguaration</h3>
                   <time className="block mb-2 pt-2 text-base font-normal leading-none text-black">4th to 9th MAY</time>
-                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br/>PRIZE POOL: RS. 80,000</p>
+                  <p className="text-lg font-normal text-white pt-2">TECHNO MAIN SALT LAKE <br />PRIZE POOL: RS. 80,000</p>
                 </div>
               </li>
             </ol>
