f5a792dd9e07e740ad3751d13e15be516ef8249b
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-04-22 12:01:18 +0530

    feat(user): update dashboard

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import Ticket from "@/app/components/Ticket";
 import { useAuthContext } from "@/context/AuthContext";
-import { getUserData } from "@/lib/firestore";
+import { countReferrals, getUserData } from "@/lib/firestore";
 import { UserData } from "@/lib/types";
 import { User } from "firebase/auth";
 import { useRouter } from "next/navigation";
@@ -11,6 +11,7 @@ import React from "react";
 export default function DashboardPage() {
   const { user } = useAuthContext() as { user: User | null };
   const [userData, setUserData] = React.useState<UserData | null>(null);
+  const [referrals, setReferrals] = React.useState(0);
 
   const router = useRouter();
 
@@ -24,13 +25,16 @@ export default function DashboardPage() {
             router.push("/new-user");
           }
         });
+        countReferrals("users", `${userData?.evgId}`).then((doc) => {
+          setReferrals(doc.data().count);
+        });
       } else {
         router.push("/");
       }
     } catch (error) {
       console.log(error);
     }
-  }, [router, user]);
+  }, [router, user, userData?.evgId]);
 
   return (
     <>
@@ -39,6 +43,11 @@ export default function DashboardPage() {
         <h1 className="pb-4 text-center text-2xl font-bold">Welcome {userData?.name},</h1>
         <div className="flex flex-col justify-center gap-y-4">
           <Ticket userData={userData} />
+          <div className="mx-auto flex gap-x-4">
+            <div className="rounded-lg border-4 border-blue-600 bg-white p-2 px-4">
+              Referrals: <b>{referrals}</b>
+            </div>
+          </div>
         </div>
       </div>
     </>
